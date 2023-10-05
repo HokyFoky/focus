fb16dccda2bd22f7d60ecc578f4f21f26d5a48c8
ianchae <dldksco@naver.com>
2023-10-05 14:25:51 +0900

    first commit

---
+++ src/main/java/com/tmax/dbas/DbasApplication.java
@@ -0,0 +1,13 @@
+package com.tmax.dbas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmax/dbas/DbasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmax.dbas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
